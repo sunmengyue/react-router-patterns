62c0adc7f96f9980308820ea15a7d5a6bc48a209
Simon <bardrabbit@gmail.com>
2020-10-24 13:51:45 -0400

    add a simple search form

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Food from './Food';
 import Meal from './Meal';
+import SearchFood from './SearchFood';
 import { Route, Switch } from 'react-router-dom';
 
 function App() {
@@ -8,13 +9,15 @@ function App() {
     <div className="App">
       {/* <Route exact path="/food/:name" component={Food} /> */}
       <Switch>
-        <Route render={() => <h1>Error! Page Not Found</h1>} />
         <Route
           exact
           path="/food/:name"
           render={(routeProps) => <Food {...routeProps} />}
         />
+        as
+        <Route exact path="/" render={() => <SearchFood />} />
         <Route exact path="/food/:foodName/drink/:drinkName" component={Meal} />
+        <Route render={() => <h1>Error! Page Not Found</h1>} />
       </Switch>
     </div>
   );
