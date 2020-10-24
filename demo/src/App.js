c998dfdea857bf84ebc57b63a58828e98b08845f
Simon <bardrabbit@gmail.com>
2020-10-23 22:29:39 -0400

    building the skeleton of the project

---
+++ demo/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Food from './Food';
+
+function App() {
+  return (
+    <div className="App">
+      <Food name="kale" />
+    </div>
+  );
+}
+
+export default App;
---
+++ demo/src/Food.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Food extends Component {
+  render() {
+    const url = `https://source.unsplash.com/1600x900/?${this.props.name}`;
+    return (
+      <div className="Food">
+        <h1>I love to eat {this.props.name}</h1>
+        <img src={url} alt="{this.props.name}" />
+      </div>
+    );
+  }
+}
+
+export default Food;
